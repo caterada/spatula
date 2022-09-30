2b6a483779bb0d2d4afca38d484cbfdd37f4b774
caterada <caterada@gmail.com>
2022-09-30 13:05:03 -0700

    render 12 random recipe images, add Header.css

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,17 +1,29 @@
 import { useEffect, useState } from 'react';
 
 function Results(props) {
+	//save results into useState
 	const [recipes, setRecipes] = useState([]);
 	useEffect(() => {
 		const url = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOON_KEY}&number=12`;
 		fetch(url)
 			.then((res) => res.json())
-			.then((res) => {
-				console.log(res);
+			.then((data) => {
+				console.log(data);
+				setRecipes(data.recipes);
 			})
 			.catch(console.error);
 	}, []);
-	return <div>Results</div>;
+	return (
+		<div>
+			{recipes.map((recipe) => {
+				return (
+					<div key={recipe.id}>
+						<img src={recipe.image} alt={recipe.title} />
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default Results;
