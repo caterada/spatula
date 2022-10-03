c5d1d301a394716162a434ae40fc8c97044f71fe
caterada <caterada@gmail.com>
2022-10-03 13:20:26 -0700

    test with one object data pulled into json, map out instructions on recipe page

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,21 +1,27 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import './Recipe.css';
+import data from '../Object.json';
 
-function Recipe(props) {
+function Recipe() {
 	const [recipe, setRecipe] = useState(null);
-	let params = useParams();
+	// let params = useParams();
+
+	// useEffect(() => {
+	// 	fetch(
+	// 		`https://api.spoonacular.com/recipes/${params.id}/information?apiKey=${process.env.REACT_APP_SPOON_KEY}`
+	// 	)
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			console.log(data);
+	// 			setRecipe(data);
+	// 		})
+	// 		.catch(console.error);
+	// }, [params.name]);
 
 	useEffect(() => {
-		fetch(
-			`https://api.spoonacular.com/recipes/${params.id}/information?apiKey=${process.env.REACT_APP_SPOON_KEY}`
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				setRecipe(data);
-			})
-			.catch(console.error);
-	}, [params.name]);
+		setRecipe(data);
+		// console.log(data);
+	});
 
 	//render when recipe is null
 	if (!recipe) {
@@ -25,8 +31,22 @@ function Recipe(props) {
 	//render normal return
 	return (
 		<div className='details-container'>
-			<img src={recipe.image} alt={recipe.title} />
-			<div className='details'>{recipe.title}</div>
+			<div className='recipe-image'>
+				<img src={data.image} alt={data.title} />
+			</div>
+			<h1 className='recipe-title'>{data.title}</h1>
+			<section className='data-instructions'>
+				{data.analyzedInstructions[0].steps.map((steps, index) => (
+					<div key={index}>
+						<span key={steps.number}>
+							<ul className='step' key={steps.step}>
+								{steps.number}
+								{'.'} {steps.step}
+							</ul>
+						</span>
+					</div>
+				))}
+			</section>
 		</div>
 	);
 }
