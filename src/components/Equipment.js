87fbac41ccf17af4e5c88c5c93008b8b201ec2d4
caterada <caterada@gmail.com>
2022-10-04 11:20:52 -0700

    get equipment data to render

--- src/components/Equipment.js
+++ src/components/Equipment.js
@@ -4,35 +4,27 @@ import { useEffect, useState } from 'react';
 function Equipment({ id }) {
 	const [equipments, setEquipments] = useState('');
 
-	const url = `https://api.spoonacular.com/recipes/${id}/equipmentWidget.json?apiKey=${process.env.REACT_APP_SPOON_KEY}`;
-
 	useEffect(() => {
-		fetchData();
-	}, []);
-	// useEffect(() => {
-	// 	setEquipments(equipments);
-	// }, []);
-
-	// fetch data is an array, not sure how to grab details
-
-	const fetchData = () => {
-		fetch(url)
+		fetch(
+			`https://api.spoonacular.com/recipes/${id}/equipmentWidget.json?apiKey=${process.env.REACT_APP_SPOON_KEY}`
+		)
 			.then((res) => res.json())
 			.then((res) => {
 				console.log(res);
-				setEquipments(res);
-			});
-	};
+				setEquipments(res.equipment);
+			})
+			.catch(console.error);
+	}, []);
 
 	return (
 		<section className='equipment'>
 			<h2 className='equipment-title'>Equipment data</h2>
 			<div className='equipment-list'>
-				{/* {equipments.map((equipment, index) => (
+				{equipments.map((equipment, index) => (
 					<div key={index}>
 						<ul key={equipment.name}>{equipment.name}</ul>
 					</div>
-				))} */}
+				))}
 			</div>
 		</section>
 	);
